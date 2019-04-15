11d2538c4280e6cc6a1db26e8d45cb8b54ab7ab3
saachibhayani <43556163+saachibhayani@users.noreply.github.com>
2019-04-15 11:12:37 -0700

    Create presentstem.py

---
+++ presentstem.py
@@ -0,0 +1,12 @@
+ei = ["abstener", "abstenerse", "adherir", "adquirir", "advertir", "apretar", "arrepentir", "ascender", 
+"asentir", "atender", "atravesar", "calentar", "cerrar", "circunvenir", "comenzar", "conferir", "confesar", 
+"consentir", "contener", "contravenir", "convenir", "convertir", "defender", "descender", "desmembrar", 
+"desmentir", "despertar", "desperarse", "desplegar", "detener", "diferir", "digerir", "discernir", "disentir", 
+"divertirse", "empezar", "encender", "encerrar", "encomendar", "enmendar", "ensangrentar", "entender", "enterrar", 
+"entretener", "entrevenir", "extender", "fregar", "gobernar", "heder", "helar", "herir", "hervir", "inferir",
+"ingerir", "intervenir", "invernar", "invertir", "malherir,", "manifestar", "mantener", "mentar", "mentir", "mentirse", 
+"merendar", "negar", "nevar", "obtener", "ofender", "pensar", "perder", "pervertir", "preferir", "presentir", "prevenir"
+"querer", "recomendar", "referir", "regar", "remendar", "renegar", "replegar", "requerir", "restregar", "retener", 
+"reventar", "segar", "sembrar", "sentar", "sentir", "sentirse", "serrar", "sosegar", "sostener", "soterrar",
+"subarrender", "sugerir", "temblar", "tender", "tener", "tentar", "transferir", "tropezar", "venir", "verter"] 
+ei = set(ei)
